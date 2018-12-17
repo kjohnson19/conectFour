d8eff5c0f7cddf18401294fdbbba4e89f0856332
Kenya Johnson <kenyajohnson@PSADMINs-MacBook-Air-7.local>
2018-12-17 18:56:43 -0500

    declared all variables for onclick functions

--- app.js
+++ app.js
@@ -3,19 +3,19 @@
 var allColumns = ['a','b','c','d','e','f','g','h'];
 var allRows = ['one','two','three','four','five','six','seven',
 'eight'];
-var fadeTime = 300;
-
-var player1 = 0;
 var colors = ['yellow', 'purple'];
+var fadeTime = 420;
+var player1 = 0;
 
-$(".place").click(function(){
+$(".hole").click(function(){
+  // declare variables for the onclick event for playing game
 	var classes = ($(this).attr('class')).split(" ");
 	var thisClass = classes[classes.length - 1];
 	var belowClasses = '';
 	var thisId = $(this).parent().attr('id');
 	
 	if ((thisId == "one") == false){
-		var below = (rows[rows.indexOf(thisId) - 1]);
+		var below = (allRows[allRows.indexOf(thisId) - 1]);
 		belowClasses = ($("#" + below + " ." + thisClass + "").attr('class'));
 		belowClasses = belowClasses.split(" ");
 		if (belowClasses.indexOf("yellow") >= 0 || belowClasses.indexOf("purple") >= 0){
@@ -50,10 +50,12 @@ function rotateColor(t){
 	}, fadeTime);
 	$(t).fadeIn(fadeTime);
 }
+// ===============restart game button jquery function below============
 
 $("#red").on("click", function(){
   location.reload()
 })
 
+// fade in and fade out attributes for when page refreshes
 $("#board").fadeOut(fadeTime);
 $("#board").fadeIn(fadeTime);
